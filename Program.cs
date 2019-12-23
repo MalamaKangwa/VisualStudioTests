3f609cb374886b1d08fc80131cbc20786d34c3ee
MalamaKangwa <58172771+MalamaKangwa@users.noreply.github.com>
2019-12-23 16:59:08 +0200

    Task: Updated Hello World

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace VisualStudioTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World is the basics for us!");
         }
     }
 }
