24b67e347316c72b4349d36a293452f616d3e350
MalamaKangwa <MalamaKangwa>
2019-12-23 16:55:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
